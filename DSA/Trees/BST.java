f3d33ad259b37ec706d79718f9b42d89e7e9eb2a
AyushDixit123 <ayushdixitworkmail@gmail.com>
2024-10-23 21:20:25 +0530

    dsa on github

---
+++ DSA/Trees/BST.java
@@ -0,0 +1,73 @@
+
+public class BST {
+
+
+  private class Node{
+    private int value;
+    private Node left;
+    private Node right;
+    private int height;
+
+    public Node(int value){
+      this.value = value;
+    }
+    public int getValue(){
+      return value;
+    }
+  }
+  private Node root;
+
+  public BST(){
+  }
+
+  public int height(Node node){
+    if(node == null){
+
+      return -1;
+    }
+    return node.height;
+  }
+
+  public boolean isEmpty(){
+    return root == null;
+  }
+public void display(){
+  displayTree(root, "root node:");
+}
+
+private void displayTree(Node node, String details){
+  if(node == null){
+    return;
+  }
+  System.out.println(details + node.getValue());
+
+  display(node.left, "left child of " + node.getValue() + ":");
+  display(node.right,"right child of "+ node.getValue() + ":");
+}
+
+
+public void insert(int value){
+
+  root = insertnode(value, root);
+}
+
+private Node insertnode(int value, Node node){
+
+  if(node == null){
+    node = new Node(value);
+    return node;
+  }
+
+  if(value < node.value){
+    node.left = insertnode(value, node.left);
+  }
+
+  if(value > node.value){
+    node.right = insertnode(value, node.right)
+  }
+
+  node.height = Math.max(height(node.left), height(node.right))+1;
+  return node;  //makes sure that while returning no node is changing
+}
+}
+
---
+++ DSA/Trees/BinaryTree.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+public class BinaryTree {
+
+  public BinaryTree(){
+
+  }
+  private static class Node{
+    int value;
+    Node left;
+    Node right;
+
+    public Node(int value){
+      this.value = value;
+    }
+
+  }
+
+  private Node root;
+
+  //insert elements
+  public void insert(Scanner scanner){
+    System.out.println("Enter the root node value:");
+    int value = scanner.nextInt();
+    root = new Node(value);
+//after adding the root node we need to call another recursion function that will fill the tree
+    populate(scanner,root);
+  }
+
+
+
+  private void populate(Scanner scanner, Node node){
+
+    System.out.println("Do you want to enter value left of" + node.value );
+
+    boolean left = scanner.nextBoolean();
+
+    if(left){
+      System.out.println("Enter the value to left of " + node.value);
+      int value = scanner.nextInt();
+      node.left = new Node(value);
+      populate(scanner,node.left);
+    }
+
+
+    System.out.println("Do you want to enter value right of" + node.value );
+
+    boolean right = scanner.nextBoolean();
+
+    if(right){
+      System.out.println("Enter the value to right of " + node.value);
+
+      int value = scanner.nextInt();
+      node.right = new Node(value);
+      populate(scanner,node.right);
+    }
+  }
+
+
+  public void displayTree(){
+    display(root,"");
+  }
+  private void display(Node node, String indent){
+
+    if(node == null){
+      return;
+    }
+
+        System.out.println(indent + node.value);
+        display(node.left, indent + "\t");
+        display(node.right, indent + "\t");
+
+  }
+}
+
